fdae64c2c2ee513095b3db1d1dd2e15034eeefce
devbyaccident <github@charliebravo.33mail.com>
2021-10-24 15:55:37 -0400

    Final edits to slides and print-users.sh

--- student-environments/print-users.sh
+++ student-environments/print-users.sh
@@ -4,14 +4,13 @@ values=$(terraform output -json)
 let i=0
 for username in $(echo $values | jq -r '.students.value[].name'); do
   for loop in 1; do
-	echo "Instructions repo:     https://github.com/DevelopIntelligenceBoulder/Terraform"
+	echo "Instructions repo:     https://github.com/devbyaccident/Introduction-to-Terraform"
 	echo "Console URL:           https://481877421625.signin.aws.amazon.com/console"
 	echo "Username/Alias:        $username"
 	password=$(echo $values | jq -r '.passwords.value[]['"$i"']' | base64 --decode | gpg -dq)
 	echo "AWS Console Password:  $password"
 	region=$(echo $values | jq -r '.students.value['"$i"'].region')
 	echo "Exercise 11 Region:    $region"
-	echo "Link to the slides:    http://bit.ly/terra-forma"
 	echo "Instructor email:      chris@devbyaccident.com"
 	#echo "Course Evaluation:     $(cat survey-link)"
 	echo ""
